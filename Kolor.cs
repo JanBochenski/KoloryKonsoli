055f9a90358bcded886f1bf9b217f6c57f663fd0
jboch <jboch@JANKOWY>
2024-05-20 11:31:26 +0200

    Dodaj pliki projektów.

---
+++ Kolor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoloryKonsoli
+{
+    internal class Kolor
+    {
+        private ConsoleColor naszKolor;
+        private int? naszRodzaj = 0;
+
+        public Kolor(string name,int? rodzaj)
+        {
+            rodzaj = ((rodzaj==null)?0:rodzaj);
+            this.naszRodzaj = rodzaj;
+            this.ZmienKolor(name);
+        }
+        public ConsoleColor DajKolor()
+        {
+            return this.naszKolor;
+        }
+
+        public void ZmienKolor(string name)
+        {
+            int indeks;
+            bool jestWynik = false;
+            ConsoleColor staryKolorKonsoli;
+            string[] kolory = {"czarny","ciemnoniebieski","ciemnozielony","ciemnoturkusowy","ciemnoczerwony","filoletowy","ciemnożółty","szary","ciemnoszary","niebieski","zielony","turkusowy","czerwony","różowy","żółty","biały" };
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            if (!String.IsNullOrWhiteSpace(name))
+            {
+                name = name.ToLower();
+                foreach (var color in colors)
+                {
+                    if (name == color.ToString().ToLower())
+                    {
+                        this.naszKolor = color;
+                        jestWynik = true;
+                    }
+                }
+                int i = 0;
+
+                // długość tablicy kolory i colors identyczna
+
+                foreach (var kolor in kolory)
+                {
+                    if (name == kolor.ToLower())
+                    {
+                        this.naszKolor = colors[i];
+                        jestWynik = true;
+                    }
+                    i++;
+                }
+                if (!jestWynik && Char.IsNumber(name, 0))
+                {
+                    indeks = Convert.ToUInt16(name);
+                    if (0 <= indeks && indeks <= colors.Length - 1)
+                        this.naszKolor = colors[indeks];
+                    jestWynik = true;
+                }
+                if (!jestWynik)
+                {
+                    Console.WriteLine("Możliwe kolory:");
+                    i = 0;
+                    foreach (var color in colors)
+                    {
+                        Console.ForegroundColor = color;
+                        staryKolorKonsoli = Console.BackgroundColor;
+                        if (Console.ForegroundColor == Console.BackgroundColor)
+                        {            
+                            Console.BackgroundColor = ((i==0)?colors[colors.Length-1]:colors[i-1]);
+                        }
+                        Console.WriteLine(color.ToString() + "(" + kolory[i] + ")");
+                        Console.BackgroundColor = staryKolorKonsoli;
+                        i++;
+                    }
+                    Console.ResetColor();
+                    if (this.naszRodzaj == 0)
+                        this.naszKolor = Console.ForegroundColor;
+                    else
+                        this.naszKolor = Console.BackgroundColor;
+                }
+            }
+            else
+            {
+                Console.ResetColor();
+                if (this.naszRodzaj == 0)
+                    this.naszKolor = Console.ForegroundColor;
+                else
+                    this.naszKolor = Console.BackgroundColor;
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoloryKonsoli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kolorTekstu="", kolorTla="";
+            Kolor kolor1 = new Kolor(Console.ForegroundColor.ToString(), 0);
+            Kolor kolor2 = new Kolor(Console.BackgroundColor.ToString(), 1);
+            while (true)
+            {
+                Console.Write("Podaj proszę kolor tekstu: ");
+                kolorTekstu = Console.ReadLine();
+                kolor1.ZmienKolor(kolorTekstu);
+                Console.Write("Podaj proszę kolor tła: ");
+                kolorTla = Console.ReadLine();
+                kolor2.ZmienKolor(kolorTla);
+
+                Console.ForegroundColor = kolor1.DajKolor();
+                Console.BackgroundColor = kolor2.DajKolor();
+
+                Console.WriteLine("Wybrałeś Kolor tekstu: " + kolorTekstu);
+                Console.WriteLine("Wybrałeś Kolor tła: " + kolorTla);
+                Console.WriteLine("Proszę napisz coś, zakończ klawiszem <Enter>");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
